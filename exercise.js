e1b71597c1baed227b75796e9555270f50b4b06c
dhruvipanchal007 <133941629+dhruvipanchal007@users.noreply.github.com>
2023-08-05 12:22:10 +0530

    2 files modified

---
+++ exercise.js
@@ -0,0 +1,93 @@
+// {exercise1}
+// let name="dhruvi";
+
+
+// {exe2}
+// let age=20;
+
+
+// {ex 3}
+// let strnumber="42";
+// let age=Number(strnumber);
+
+
+// {ex 4}
+// let x=5;
+// let y=7;
+// console.log(x+y);
+
+// {ex 5}
+// let m=10
+// let n=3
+// console.log(m-n);
+
+// {ex 6}
+// let x=4
+// let y=6
+// console.log(x*y);
+
+// {ex 7}
+// let p=15
+// let q=3
+// console.log(p/q);
+
+// {ex 8}
+// let j=10
+// let k=3
+// console.log(j % k);
+
+// {ex 9}
+// let a="4"
+// let b="2"
+// console.log(a>b)
+
+// {ex 10}
+// let h="apple"
+// let k="banana"
+// console.log(h>k)
+
+
+// {ex=11}
+// let ismorning=true;
+// if(ismorning){
+  // console.log('hello')
+// }
+
+// {ex=12}
+// let isEvening=true
+// if(isEvening){
+  // console.log('goodbye')
+// }
+
+// {ex=13}
+// let x=2
+// let y=3
+// if(x > 0 && y>0){
+  // console.log('both greater than zero')
+// }
+
+// {ex=14}
+// let a=5
+// let b=10
+// if(a===10 || b===10){
+  // console .log('a or b is eual to 10')
+// }
+
+// {ex =15}
+// let x=7
+// let y=5
+// let z=8
+// if(x > y || z < 10)
+// console.log('x grater than y')
+
+// ex= 16
+// let x;
+// x=('"Hello,world!"')
+// alert(x)
+
+// ex=17
+// let username= prompt('what is your name?')
+// console.log(username)
+
+
+
